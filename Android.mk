d549b0ae3a844651576c01a84894ecfbb7f0f8f0
cr5315 <cr5315@cr5315.com>
2012-08-11 20:25:37 -0500

    Changed 'FNV Wallpapers' to 'FNVWallpapers'
    Signed-off-by: cr5315 <cr5315@cr5315.com>

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files) $(call all-renderscript-files-under, src)
 
-LOCAL_PACKAGE_NAME := FNV Wallpapers
+LOCAL_PACKAGE_NAME := FNVWallpapers
 LOCAL_CERTIFICATE := shared
 
 include $(BUILD_PACKAGE)
